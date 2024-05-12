71add9db8d6073e4c145da03d03670bea154689e
Dipanwita Biswas <biswasdipanwita.2001@gmail.com>
2024-05-12 22:59:06 +0530

    initiazed

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const HomePage = () => {
+  return (
+    <>
+        <Navbar/>
+      <h1 className='bg-black text-white '>This is the home page</h1>
+    </>
+  )
+}
+
+export default HomePage
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Navbar() {
+  return (
+    <>
+      <div>
+        <MenuIcon />
+      </div>
+    </>
+  )
+}
+
+export default Navbar
