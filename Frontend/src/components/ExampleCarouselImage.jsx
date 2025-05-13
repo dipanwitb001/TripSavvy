c4b7543d558b83f7a1eb19c17355fd35f8b9ea5c
Dipanwita Biswas <biswasdipanwita.2001@gmail.com>
2025-05-14 00:42:27 +0530

    homepage is responsive

--- Frontend/src/components/ExampleCarouselImage.jsx
+++ Frontend/src/components/ExampleCarouselImage.jsx
@@ -1,19 +1,36 @@
-// src/components/ExampleCarouselImage.jsx
+// // src/components/ExampleCarouselImage.jsx
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// const ExampleCarouselImage = ({ text, image }) => {
+//   return (
+//     <div>
+//       <img
+//         className="d-block  w-full h-auto object-cover"
+//         src={image}
+//         alt={text}
+//       />
+//       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+//       <div className="carousel-caption d-none d-md-block ">
+//         {/* <h5>{text}</h5> */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ExampleCarouselImage;
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ExampleCarouselImage = ({ text, image }) => {
   return (
-    <div>
+    <div className="relative w-full h-screen">
       <img
-        className="d-block  w-full h-auto object-cover"
+        className="w-full h-full object-cover"
         src={image}
         alt={text}
       />
-      <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-      <div className="carousel-caption d-none d-md-block ">
-        {/* <h5>{text}</h5> */}
-      </div>
+      <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent"></div>
     </div>
   );
 }
